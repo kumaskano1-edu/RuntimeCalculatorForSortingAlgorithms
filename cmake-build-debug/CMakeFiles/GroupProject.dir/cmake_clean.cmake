85ab0db0a89bc5c74b93968d37b8a93004840157
Kurmanbek <kumakaraev01@gmail.com>
2020-06-10 23:23:15 -0700

    my first commit

---
+++ cmake-build-debug/CMakeFiles/GroupProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GroupProject.dir/main.cpp.obj"
+  "GroupProject.exe"
+  "GroupProject.exe.manifest"
+  "GroupProject.pdb"
+  "libGroupProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GroupProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
